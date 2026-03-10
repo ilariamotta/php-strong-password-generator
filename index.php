ac5436ad1dbd0cb91dbbcd54a6600f5afeb83288
ilariamotta <ilaria.motta93@gmail.com>
2026-03-10 18:16:28 +0100

    feat: 1 milestone, printing password

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
-<?php var_dump($_GET["length"]); ?>
+<?php var_dump($_GET["length"]); 
+
+$passlength = $_GET["length"] ?? 8;
+$chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*<>?+-_";
+
+
+
+
+function passwordGenerator($passlength, $chars) {
+      $password = "";
+for($i = 0; $i < $passlength; $i++) {
+    $element = random_int(0, strlen($chars) - 1);
+    $password .= $chars[$element];
+}
+    return $password;
+}
+
+$password = passwordGenerator($passlength, $chars);
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -27,19 +46,19 @@
         <label for="number-input" class="block mb-2.5 text-sm font-medium text-heading italic rounded-pill">Scegli la lunghezza della tua nuova password!</label>
        <input type="number" name="length" id="number-input" min="8" max="64" required class="w-full mt-2 px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"/>
 
-        <button class="mt-4 w-36 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition">Magia sia!</button>
+        <button type="submit" class="mt-4 w-36 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition">Magia sia!</button>
         </form>
         <!-- close form -->
                 <h3 class="text-lg font-bold text-center pt-2">La tua password è pronta da utilizzare dove vuoi!</h3>
                 <div class="grid grid-cols-10 gap-2 mt-4">
             <!-- password -->
-    <div class="col-span-8 bg-white rounded p-3 text-center font-mono text-xl">
-        ...
+    <div class="col-span-6 bg-white rounded p-3 text-center font-mono text-xl">
+        <?php echo $password; ?> 
     </div>
             <!-- copy -->
-    <button class="col-span-1 bg-blue-500 text-white rounded p-3">Copy</button>
+    <button class="col-span-2 bg-blue-500 text-white rounded p-3">Copy</button>
             <!-- regenerate -->
-    <button class="col-span-1 bg-green-500 text-white rounded p-3">↻</button>
+    <button class="col-span-2 bg-green-500 text-white rounded p-3">↻</button>
 </div>
 <!-- box password -->
 </div>
