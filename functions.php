21641177cf0b0c3f2d910d9426a048d4ea9ee193
ilariamotta <ilaria.motta93@gmail.com>
2026-03-11 18:23:44 +0100

    feat:password customization (milestone 4)

--- functions.php
+++ functions.php
@@ -1,7 +1,24 @@
 <?php 
 
-function passwordGenerator($passlength, $chars) {
+function passwordGenerator($passlength, $chars, $symbols, $uppercases, $numbers) {
       $password = "";
+        $params="";
+
+if (isset($_GET["symbols"])) {
+    $params .= $symbols;
+}
+
+if (isset($_GET["uppercases"])) {
+    $params .= $uppercases;
+}
+
+if (isset($_GET["numbers"])) {
+    $params .= $numbers;
+}
+
+$chars .= $params;
+
+
 for($i = 0; $i < $passlength; $i++) {
     $element = random_int(0, strlen($chars) - 1);
     $password .= $chars[$element];
--- index.php
+++ index.php
@@ -6,11 +6,16 @@ require_once "./functions.php";
 
 
 $passlength = $_GET["length"] ?? 8;
-$chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*<>?+-_";
+
+
+$chars = "abcdefghijklmnopqrstuvwxyz";
+$symbols ="!@#$%^&*<>?+-_";
+$numbers ="0123456789";
+$uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
 if (isset($_GET["length"])) {
-    $_SESSION["password"] = passwordGenerator($passlength, $chars);
+    $_SESSION["password"] = passwordGenerator($passlength, $chars, $symbols, $uppercases, $numbers);
     header("Location: ./result.php");
     exit;
 }
@@ -45,9 +50,31 @@ if (isset($_GET["length"])) {
         <label for="number-input" class="block mb-2.5 text-sm font-medium text-heading italic rounded-pill">Scegli la lunghezza della tua nuova password!</label>
        <input type="number" name="length" id="number-input" min="8" max="64" required class="w-full mt-2 px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"/>
 
-        <button type="submit" class="mt-4 w-36 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition">Magia sia!</button>
-        </form>
-        <!-- close form -->
+        <button type="submit" class="mb-4 mt-4 w-36 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition">Magia sia!</button>
+        
+        <!-- custom password -->
+         <hr>
+         <h6 class="my-4 italic bold">Personalizza la tua password!</h6>
+        <div class="flex flex-col gap-3 mb-4">
+            <!-- uppercase letters -->
+    <label for="uppercase" class="flex items-center gap-2 text-sm font-medium text-gray-800">
+        <input id="uppercases" type="checkbox" name="uppercases" class="w-4 h-4 rounded border-gray-300 text-blue-500 focus:ring-2 focus:ring-blue-400">
+        <span>Lettere maiuscole</span>
+    </label>
+<!-- symbols -->
+    <label for="symbols" class="flex items-center gap-2 text-sm font-medium text-gray-800">
+        <input id="symbols" type="checkbox" name="symbols" class="w-4 h-4 rounded border-gray-300 text-blue-500 focus:ring-2 focus:ring-blue-400">
+        <span>Caratteri speciali</span>
+    </label>
+<!-- numbers -->
+    <label for="numbers" class="flex items-center gap-2 text-sm font-medium text-gray-800">
+        <input id="numbers" type="checkbox" name="numbers" class="w-4 h-4 rounded border-gray-300 text-blue-500 focus:ring-2 focus:ring-blue-400">
+        <span>Numeri</span>
+    </label>
+</div>
+    <!-- close form -->
+    </form>
+</div>
             
 </div>
 
